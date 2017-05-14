4cf39423339c469468f584fa3e1ab76aeee935ac
luowenqi <334974118@qq.com>
2017-05-14 17:13:52 +0800

    dev

--- Aladdin/Classes/Question/ViewController/QuestionReportVC.m
+++ Aladdin/Classes/Question/ViewController/QuestionReportVC.m
@@ -12,7 +12,7 @@
 #import "QuestionReportViewModel.h"
 
 #define kQuestionMaxLength 50
-@interface QuestionReportVC ()<UITextViewDelegate>
+@interface QuestionReportVC ()<UITextViewDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIView *bannerView;
 @property (weak, nonatomic) IBOutlet UIImageView *avatarImageV;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
@@ -24,6 +24,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *lengthLabel;
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *imagePickerButtons;
+@property (strong, nonatomic) NSMutableArray *imageDataArray;
+@property (assign, nonatomic) NSInteger pressedButtonIndex;
 
 @property (strong, nonatomic) QuestionReportViewModel *viewModel;
 @end
@@ -35,6 +37,13 @@
         _viewModel = [[QuestionReportViewModel alloc] init];
     }
     return _viewModel;
+}
+
+- (NSMutableArray *)imageDataArray {
+    if (!_imageDataArray) {
+        _imageDataArray = [NSMutableArray array];
+    }
+    return _imageDataArray;
 }
 
 - (void)viewDidLoad {
@@ -87,9 +96,104 @@
 
 - (IBAction)didPressedOnImagePickerButton:(UIButton *)sender {
     NSInteger index = [self.imagePickerButtons indexOfObject:sender];
-    NSLog(@"image button index: %d",index);
+    self.pressedButtonIndex = index;
+    
+    if (index < self.imageDataArray.count) {
+        // 提示删除操作
+        UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"提示" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@"移除图片", nil];
+        actionSheet.tag = 1000;
+        actionSheet.delegate = self;
+        [actionSheet showInView:self.view];
+        
+    }else{
+        // 提示选择图片操作
+        UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"选择图片" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@"现在拍摄", @"相册选取", nil];
+        actionSheet.tag = 900;
+        actionSheet.delegate = self;
+        [actionSheet showInView:self.view];
+    }
 }
 
+- (void)reloadImageButtons {
+    for (int i = 0; i < self.imagePickerButtons.count; i++) {
+        UIButton *button = self.imagePickerButtons[i];
+        
+        if (i < self.imageDataArray.count) {
+            UIImage *image = self.imageDataArray[i];
+            [button setImage:image forState:UIControlStateNormal];
+            button.hidden = NO;
+        }else{
+            [button setImage:[UIImage imageNamed:@"question_image_picker_button"] forState:UIControlStateNormal];
+            button.hidden = (i == self.imageDataArray.count) ? NO : YES;
+        }
+    }
+}
+
+#pragma mark - UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet willDismissWithButtonIndex:(NSInteger)buttonIndex {
+    switch (actionSheet.tag) {
+        case 1000:
+        {
+            if (buttonIndex == 0) {
+                [self.imageDataArray removeObjectAtIndex:self.pressedButtonIndex];
+                [self reloadImageButtons];
+            }
+        }
+            break;
+            
+        case 900:
+        {
+            if (buttonIndex != 0 && buttonIndex != 1) {
+                return;
+            }
+            // 1.判断相册是否可以打开
+            if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] && buttonIndex == 0) {
+                return [MBProgressHUD showAutoMessage:@"没有设置访问相机权限"];
+            }
+            
+            if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary] && buttonIndex == 1) {
+                return [MBProgressHUD showAutoMessage:@"没有设置访问相册权限"];
+            }
+            
+            // 2. 创建图片选择控制器
+            UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+            imagePickerController.delegate = self;
+            
+            if (buttonIndex == 0) {
+                imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+            }else if (buttonIndex == 1) {
+                imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+            }
+            [self presentViewController:imagePickerController animated:YES completion:nil];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+#pragma mark - UINavigationControllerDelegate
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    [navigationController.navigationBar setBarTintColor:GLOBAL_TINT_COLOR];
+    NSDictionary * dict = [NSDictionary dictionaryWithObject:[UIColor whiteColor] forKey:NSForegroundColorAttributeName];
+    navigationController.navigationBar.titleTextAttributes = dict;
+}
+
+#pragma mark - UIImagePickerController
+// 获取图片后的操作
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    // 销毁控制器
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    // 设置图片
+    __block UIImage *image = nil;
+    image = [info[UIImagePickerControllerEditedImage] copy];
+    image = [info[UIImagePickerControllerOriginalImage] copy];
+
+    NSParameterAssert(image);
+    [self.imageDataArray addObject:image];
+    [self reloadImageButtons];
+}
 
 #pragma mark - UITextViewDelegate
 - (void)textViewDidChange:(UITextView *)textView {
